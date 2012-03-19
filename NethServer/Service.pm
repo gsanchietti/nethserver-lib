b91cc069475d065d1091c970784dc0a2e5399e6b
Davide Principi <davide.principi@nethesis.it>
2012-03-19 16:50:44 +0100

    Service module: added service control module.

---
+++ NethServer/Service.pm
@@ -0,0 +1,34 @@
+#
+# NethServer Service
+#
+# Copyright (C) 2012 Nethesis srl
+#
+
+package NethServer::Service;
+
+use strict;
+
+=head1 NAME
+
+NethServer::Servicem module
+
+=cut
+
+=head2 start($daemon)
+    
+Start $daemon if it is stopped
+
+=cut
+sub start
+{
+    my $service = '/sbin/service';
+    my $daemon = shift;
+
+    qx($service $daemon status >/dev/null);
+    if ( $? != 0 ) {
+	qx($service $daemon start);
+    }
+    return $?;
+}
+
+1;
