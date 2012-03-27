c95bea0fa212a1dfc7a8ea600852218790d1ddce
Davide Principi <davide.principi@nethesis.it>
2012-03-27 12:35:01 +0200

    NethServer::Service package: added is_enabled() function

--- NethServer/Service.pm
+++ NethServer/Service.pm
@@ -7,6 +7,7 @@
 package NethServer::Service;
 
 use strict;
+use esmith::ConfigDB;
 
 =head1 NAME
 
@@ -30,5 +31,33 @@ sub start
     }
     return ($? == 0 ? 1 : 0);
 }
+
+
+=head2 is_enabled($daemon)
+
+Check if $daemon is enabled in the current runlevel. 
+
+  if(is_enabled($daemon)) {
+     start($daemon);
+  }
+
+=cut
+sub is_enabled($daemon)
+{
+    my $configurationDb = esmith::ConfigDB->open_ro();
+    my $status = $configurationDb->get_prop($daemon, 'status') || 'unknown';
+    my %runlevels = map { $_ => 1 } split(',' $configurationDb->get_prop($daemon, 'Runlevels'));
+
+    my $currentRunlevel = qx('/sbin/runlevel');
+    chomp($currentRunlevel);
+    $currentRunlevel = [split(' ', $currentRunlevel)]->[1];
+
+    if(%runlevels{$currentRunlevel} == 1) {
+	return 1;
+    }
+
+    return 0;
+}
+
 
 1;
