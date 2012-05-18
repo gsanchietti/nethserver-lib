0a491eac3b47a91c4139bcb371f9c4a3d160192e
Davide Principi <davide.principi@nethesis.it>
2012-05-18 18:08:56 +0200

    Service.pm: added manage_{add,remove} functions. Refs #1043 - Handling managedBy prop in callback actions

--- NethServer/Service.pm
+++ NethServer/Service.pm
@@ -55,7 +55,7 @@ sub stop
 
 =head2 is_enabled($daemon)
 
-Check if $daemon is enabled in the current runlevel. 
+Check if $daemon is enabled in the current runlevel. Example
 
   if(is_enabled($daemon)) {
      start($daemon);
@@ -67,8 +67,14 @@ sub is_enabled
     my $daemon = shift;
     my $configurationDb = esmith::ConfigDB->open_ro();
     my $status = $configurationDb->get_prop($daemon, 'status') || 'unknown';
+    my @managedBy = split(',', $configurationDb->get_prop($daemon, 'managedBy') || '');
 
     if($status ne 'enabled') {
+	return 0;
+    }
+
+    # Refs #1043. If a service is not managed by any package it is "disabled"
+    if( ! @managedBy) {
 	return 0;
     }
 
@@ -85,5 +91,55 @@ sub is_enabled
     return 0;
 }
 
+=head2 manage_add($package, $service [, $service])
+
+Mark $service as being managed by the given $package. Example, let
+pkg1 manage svc1 and svc2
+
+  manage_add('pkg1', 'svc1', 'svc2');
+
+=cut 
+sub manage_add
+{
+    my $packageName = shift;
+
+    my $configDb = esmith::ConfigDB->open();
+
+    foreach(@_) {
+	my $serviceRecord = $configDb->get($_);
+
+	if($configDb->prop('type') eq 'service') {	    
+	    my %packageMap = map { $_ ne '' ? ($_ => 1) : () } split(',', $serviceRecord->prop('managedBy')), $packageName;
+	    $configDb->set_prop('managedBy', join(',', keys %packageMap));
+	} else {
+	    warn "Unknown service key: $_, skipping";
+	}
+    }
+}
+
+=head2 manage_remove($package, $service [, $service])
+
+Mark $service as being no more managed by the given $package. Example,
+let pkg1 manage svc1 and svc2 anymore
+
+  manage_remove('pkg1', 'svc1', 'svc2');
+
+=cut 
+sub manage_remove
+{
+    my $packageName = shift;
+
+    my $configDb = esmith::ConfigDB->open();
+
+    foreach(@_) {
+	my $serviceRecord = $configDb->get($_);
+	if($configDb->prop('type') eq 'service') {	    
+	    my @packageList = grep { $_ ne $packageName && $_ ne '' } split(',', $serviceRecord->prop('managedBy'));
+	    $configDb->set_prop('managedBy', join(',', @packageList));
+	} else {
+	    warn "Unknown service key: $_, skipping";
+	}
+    }
+}
 
 1;
