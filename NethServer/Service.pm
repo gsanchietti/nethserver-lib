a4628d7d6c83f2e86f92a68d0324c70f9dc4f995
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-04-15 10:31:48 +0200

    Service.pm: remove managedBy implementation. Refs #1870 #1874

--- NethServer/Service.pm
+++ NethServer/Service.pm
@@ -85,14 +85,8 @@ sub is_enabled
     my $daemon = shift;
     my $configurationDb = esmith::ConfigDB->open_ro();
     my $status = $configurationDb->get_prop($daemon, 'status') || 'unknown';
-    my @managedBy = split(',', $configurationDb->get_prop($daemon, 'managedBy') || '');
 
     if($status ne 'enabled') {
-	return 0;
-    }
-
-    # Refs #1043. If a service is not managed by any package it is "disabled"
-    if( ! @managedBy) {
 	return 0;
     }
 
@@ -107,57 +101,6 @@ sub is_enabled
     }
 
     return 0;
-}
-
-=head2 manage_add($package, $service [, $service])
-
-Mark $service as being managed by the given $package. Example, let
-pkg1 manage svc1 and svc2
-
-  manage_add('pkg1', 'svc1', 'svc2');
-
-=cut 
-sub manage_add
-{
-    my $packageName = shift;
-
-    my $configDb = esmith::ConfigDB->open();
-
-    foreach(@_) {
-	my $serviceRecord = $configDb->get($_);
-
-	if($serviceRecord->prop('type') eq 'service') {	    
-	    my %packageMap = map { $_ ne '' ? ($_ => 1) : () } split(',', $serviceRecord->prop('managedBy')), $packageName;
-	    $serviceRecord->set_prop('managedBy', join(',', keys %packageMap));
-	} else {
-	    warn "Unknown service key: $_, skipping";
-	}
-    }
-}
-
-=head2 manage_remove($package, $service [, $service])
-
-Mark $service as being no more managed by the given $package. Example,
-let pkg1 manage svc1 and svc2 anymore
-
-  manage_remove('pkg1', 'svc1', 'svc2');
-
-=cut 
-sub manage_remove
-{
-    my $packageName = shift;
-
-    my $configDb = esmith::ConfigDB->open();
-
-    foreach(@_) {
-	my $serviceRecord = $configDb->get($_);
-	if($serviceRecord->prop('type') eq 'service') {	    
-	    my @packageList = grep { $_ ne $packageName && $_ ne '' } split(',', $serviceRecord->prop('managedBy'));
-	    $serviceRecord->set_prop('managedBy', join(',', @packageList));
-	} else {
-	    warn "Unknown service key: $_, skipping";
-	}
-    }
 }
 
 1;
