7e45a120f73d4a7e0e930f9febe023e6bd29b16d
Davide Principi <davide.principi@nethesis.it>
2016-03-07 15:57:14 +0100

    Expose realmd DBus properties on esmith::DB API

---
+++ lib/perl/NethServer/Database/Realmd.pm
@@ -0,0 +1,82 @@
+#
+# Copyright (C) 2016 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+use strict;
+package NethServer::Database::Realmd;
+
+use Net::DBus;
+
+sub TIEHASH {
+    my $class = shift;
+
+    my $bus = Net::DBus->system();
+
+    my $self = {
+        'bus' => $bus,
+        'service' => $bus->get_service("org.freedesktop.realmd"),
+        'keys' => [],
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub FETCH {
+    my $self = shift;
+    my $key = shift;
+
+    if( ! $self->EXISTS($key)) {
+        return undef;
+    }
+
+    my $realm = $self->{'service'}->get_object($key);
+    my $value = 'realmd';
+
+    foreach (qw(RealmName DomainName Name )) {
+        $value .= '|' . $_ . '|' . $realm->$_;
+    }
+
+    return $value;
+}
+
+sub EXISTS {
+    my $self = shift;
+    my $key = shift;
+    return grep { $_ eq $key } @{$self->_get_keys()};
+}
+
+sub FIRSTKEY {
+    my $self = shift;
+    $self->{'keys'} = $self->_get_keys();
+    return $self->NEXTKEY();
+}
+
+sub NEXTKEY {
+    my $self = shift;
+    my ($k, $v) = each($self->{'keys'});
+    return $v;
+}
+
+sub _get_keys {
+    my $self = shift;
+    my $key = shift;
+    return $self->{'service'}->get_object("/org/freedesktop/realmd/Sssd")->Realms;
+}
+
+1;
